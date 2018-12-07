fb80a0c08b490dbcef31d41d67c7f938cef0f3c8
abc515952853 <123456789@qq.com>
2018-12-07 18:36:08 +0800

    修改客户简称

---
+++ testcase/CustomerUpdateName.py
@@ -0,0 +1,45 @@
+import unittest
+import ddt
+from common import ReadExcl,ReadDB
+import ReadConfig 
+import requests
+import json 
+import uuid
+
+api='api/Customer/{0}/name'
+sheet_name = "CustomerUpdateName"
+
+excel = ReadExcl.Xlrd()
+
+@ddt.ddt
+class CustomerUpdateName(unittest.TestCase): 
+    @ddt.data(*excel.get_xls_next(sheet_name))
+    def test_CustomerUpdateName(self,data):
+        name = str(data["name"])
+        customertab = str(data["customertab"])
+        case_describe = str(data["case_describe"])
+
+        readconfig=ReadConfig.ReadConfig()
+        readdb = ReadDB.Pyodbc()
+
+        correlationid = readconfig.get_customer(customertab)
+        url = readconfig.get_url('url')+api.format(correlationid)
+        session =  readconfig.get_member('session')
+        requestid = str(uuid.uuid1())
+        headers = {'Content-Type': "application/json",'Authorization':session,"x-requestid":requestid}
+        payload ={
+            "name":name
+        }
+        r = requests.post(url=url,data = json.dumps(payload),headers = headers)
+
+        #处理请求数据到excl用例文件
+        excel.set_cell(sheet_name,int(data["case_id"]),excel.get_sheet_colname(sheet_name)["result_code"],r.status_code,excel.set_color(r.status_code))
+        excel.set_cell(sheet_name,int(data["case_id"]),excel.get_sheet_colname(sheet_name)["result_msg"],r.text,excel.set_color())
+        excel.save()
+        
+        #数据对比
+        if r.status_code==200:
+            customerdetails = readdb.GetCustomerDetailsinfo(correlationid)
+            self.assertEqual(customerdetails['name'],name,case_describe)
+        else:
+            self.assertEqual(r.status_code,200,case_describe)   
