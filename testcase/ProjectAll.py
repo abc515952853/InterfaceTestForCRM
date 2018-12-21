5ba31223541ae4f2dc7fc8495b28421cf9c8ccfc
abc515952853 <123456789@qq.com>
2018-12-21 19:21:47 +0800

    11

---
+++ testcase/ProjectAll.py
@@ -0,0 +1,38 @@
+import unittest
+import ddt
+from common import ReadExcl,ReadDB
+import ReadConfig 
+import requests
+import json 
+import uuid
+
+api='api/Project/{id}/all'
+sheet_name = "ProjectAll"
+
+excel = ReadExcl.Xlrd()
+
+@ddt.ddt
+class ProjectAll(unittest.TestCase): 
+    @ddt.data(*excel.get_xls_next(sheet_name))
+    def test_ProjectAll(self,data):
+        # key = str(data["key"])
+        # departmentId = str(data["departmentId"])
+        # case_describe = str(data["case_describe"])
+
+        # readconfig=ReadConfig.ReadConfig()
+        # readdb = ReadDB.Pyodbc()
+
+        # url = readconfig.get_url('url')+api.format(key,departmentId)
+        # session =  readconfig.get_member('session')
+        # headers = {'Content-Type': "application/json",'Authorization':session}
+        # r = requests.get(url=url, headers = headers)
+        # if r.status_code==200:
+        #     customerindepartmentid = readdb.GetCustomerInDepartmentinfo(key,departmentId)
+        #     responecustomerindepartmentid = []
+        #     for i in range(len(r.json()['list'])):
+        #         responecustomerindepartmentid.append(r.json()['list'][i]['id'])
+        #         self.assertIn(r.json()['list'][i]['id'].upper(),customerindepartmentid,case_describe)
+        #     self.assertEqual(len(responecustomerindepartmentid),len(customerindepartmentid),case_describe)
+        #     self.assertEqual(r.json()['count'],len(customerindepartmentid),case_describe)
+        # else:
+        #     self.assertEqual(r.status_code,200,case_describe)   
