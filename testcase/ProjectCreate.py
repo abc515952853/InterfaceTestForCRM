205b55505847811e1163c42588844cd6c1a8037d
abc515952853 <123456789@qq.com>
2018-12-21 13:28:54 +0800

    11

--- testcase/ProjectCreate.py
+++ testcase/ProjectCreate.py
@@ -77,7 +77,6 @@ class ProjectCreate(unittest.TestCase):
         requestid = str(uuid.uuid1())
         headers = {'Content-Type': "application/json",'Authorization':session,"x-requestid":requestid}
         r = requests.post(url=url,data = json.dumps(payload),headers = headers)
-        print(r.status_code,r.json())
         #处理请求数据到excl用例文件
         excel.set_cell(sheet_name,int(data["case_id"]),excel.get_sheet_colname(sheet_name)["result_code"],r.status_code,excel.set_color(r.status_code))
         excel.set_cell(sheet_name,int(data["case_id"]),excel.get_sheet_colname(sheet_name)["result_msg"],r.text,excel.set_color())
