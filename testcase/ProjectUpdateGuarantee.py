c6d2e838e3938469fc55399d3a600339f3a01275
abc515952853 <123456789@qq.com>
2018-12-21 10:59:30 +0800

    项目修改接口测试

---
+++ testcase/ProjectUpdateGuarantee.py
@@ -0,0 +1,47 @@
+import unittest
+import ddt
+from common import ReadExcl,ReadDB
+import ReadConfig 
+import requests
+import json 
+import uuid
+
+api='api/Project/{0}/Guarantee'
+sheet_name = "ProjectUpdateGuarantee"
+
+excel = ReadExcl.Xlrd()
+
+@ddt.ddt
+class ProjectUpdateGuarantee(unittest.TestCase): 
+    @ddt.data(*excel.get_xls_next(sheet_name))
+    def test_ProjectUpdateGuarantee(self,data):
+        guarantee = str(data["guarantee"])
+        department = str(data["department"])
+        case_describe = str(data["case_describe"])
+        expected_code = int(data["expected_code"])
+
+        readconfig=ReadConfig.ReadConfig()
+        readdb = ReadDB.Pyodbc()
+
+        projectid = readconfig.get_project('projectfactoringid')
+
+        url = readconfig.get_url('url')+api.format(projectid)
+        session =  readconfig.get_member('session')
+        requestid = str(uuid.uuid1())
+        headers = {'Content-Type': "application/json",'Authorization':session,"x-requestid":requestid}
+        payload ={
+            "guarantee":guarantee
+        }
+        r = requests.post(url=url,data = json.dumps(payload),headers = headers)
+
+        #处理请求数据到excl用例文件
+        excel.set_cell(sheet_name,int(data["case_id"]),excel.get_sheet_colname(sheet_name)["result_code"],r.status_code,excel.set_color(r.status_code))
+        excel.set_cell(sheet_name,int(data["case_id"]),excel.get_sheet_colname(sheet_name)["result_msg"],r.text,excel.set_color())
+        excel.save()
+        
+        #数据对比
+        if r.status_code==expected_code:
+            projectdetails = readdb.GetProjectDetailsinfo(projectid)
+            self.assertEqual(projectdetails['guarantee'],guarantee,case_describe)
+        else:
+            self.assertEqual(r.status_code,expected_code,case_describe)   
